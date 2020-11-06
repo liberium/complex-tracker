81ae553408e2a2838ff069138c6e00fc9610b77b
Sergey Filkin <sergey@MacBook.netgear.com>
2020-11-06 08:10:12 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,33 @@
+import tracker from 'simple-tracker'
+import fp from '@fingerprintjs/fingerprintjs'
+import isIncognito from 'is-incognito-mode'
+import BotDetector from './bot-detector'
+import { screenWidth, screenHeight } from './screen-dimensions'
+
+const fpAgent = await fp.load()
+const result = await fpAgent.get()
+const visitorId = result.visitorId
+
+let isPrivate
+try {
+    isPrivate = await isIncognito()
+} catch {
+    isPrivate = undefined
+}
+
+/* TODO: promisify bot detection */
+let isBot
+new BotDetector({
+    timeout: 1000,
+    callback: res => isBot = res.isBot
+})
+
+tracker.clientContext = {
+    ...tracker.clientContext,
+    visitorId,
+    isPrivate,
+    screenWidth,
+    screenHeight
+}
+
+export default tracker
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+
+module.exports = {
+  entry: "./main.js",
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  experiments: {
+      topLevelAwait: true
+  }
+}
