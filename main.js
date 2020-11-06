758280b0dd75a2c300fac714236e87fbb7bf533a
Sergey Filkin <sergey.filkin@gmail.com>
2020-11-06 23:34:24 +0300

    Add initializeTrackerClientContext()

--- main.js
+++ main.js
@@ -4,30 +4,32 @@ import isIncognito from 'is-incognito-mode'
 import BotDetector from './bot-detector'
 import { screenWidth, screenHeight } from './screen-dimensions'
 
-const fpAgent = await fp.load()
-const result = await fpAgent.get()
-const visitorId = result.visitorId
-
-let isPrivate
-try {
-    isPrivate = await isIncognito()
-} catch {
-    isPrivate = undefined
+window.initializeTrackerClientContext = async callback => {
+    const fpAgent = await fp.load()
+    const result = await fpAgent.get()
+    const visitorId = result.visitorId
+    
+    let isPrivate
+    try {
+        isPrivate = await isIncognito()
+    } catch {
+        isPrivate = undefined
+    }
+    
+    /* TODO: promisify bot detection */
+    let isBot
+    new BotDetector({
+        timeout: 1000,
+        callback: res => isBot = res.isBot
+    })
+    
+    tracker.clientContext = {
+        ...tracker.clientContext,
+        visitorId,
+        isPrivate,
+        screenWidth,
+        screenHeight
+    }
+    
+    if (typeof callback === 'function') callback()
 }
-
-/* TODO: promisify bot detection */
-let isBot
-new BotDetector({
-    timeout: 1000,
-    callback: res => isBot = res.isBot
-})
-
-tracker.clientContext = {
-    ...tracker.clientContext,
-    visitorId,
-    isPrivate,
-    screenWidth,
-    screenHeight
-}
-
-export default tracker
